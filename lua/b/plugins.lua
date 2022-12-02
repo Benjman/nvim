be0badb92860a222d04f48a4ae56a7fe951ea650
Ben <benjamin.s.record@gmail.com>
2022-12-01 20:22:52 -0700

    Plugin: hrsh7th/nvim-cmp

--- lua/b/plugins.lua
+++ lua/b/plugins.lua
@@ -96,5 +96,19 @@ return packer.startup {
     -- snippets
     use { 'L3MON4D3/LuaSnip' }
     use { 'rafamadriz/friendly-snippets' }
+
+    -- cmp
+    use {
+      'hrsh7th/nvim-cmp',
+      requires = {
+        { 'hrsh7th/cmp-buffer' },
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/cmp-nvim-lua' },
+        { 'hrsh7th/cmp-path' },
+        { 'hrsh7th/cmp-cmdline' },
+        { 'saadparwaiz1/cmp_luasnip' },
+      },
+      config = function() require 'b.plugins.nvim-cmp' end,
+    }
   end,
 }
