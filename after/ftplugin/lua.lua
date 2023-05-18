c6dca9baf7e426c748f10e038cada41e452e7798
Ben <benjamin.s.record@gmail.com>
2023-05-17 22:52:48 -0600

    LSP: Lua

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,16 @@
+if vim.fn.executable('lua-language-server') == 1 then
+  vim.lsp.start({
+    name = 'LuaLS',
+    cmd = { 'lua-language-server' },
+    settings = {
+      Lua = {
+        runtime = { version = 'LuaJIT', },
+        diagnostics = { globals = { 'vim' }, },
+        workspace = { checkThirdParty = false, },
+        telemetry = { enable = false },
+      },
+    },
+    root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]),
+    on_attach = require('b.lsp').on_attach,
+  })
+end
