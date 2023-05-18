9fd4126ef38ee5d62ec6aa6e8260c3070578a10b
Ben <benjamin.s.record@gmail.com>
2023-05-17 21:40:17 -0600

    Autocommands: TextYankPost

--- lua/b/autocommands.lua
+++ lua/b/autocommands.lua
@@ -1,1 +1,5 @@
--- stubbed file
+-- highlight on yank text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function() vim.highlight.on_yank() end,
+  group = vim.api.nvim_create_augroup('TextYankPost_hl_on_yank', { clear = true }),
+})
