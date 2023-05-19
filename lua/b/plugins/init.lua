a42048708f8e1d368ecfdf62e7ac895fce8a7f93
Ben <benjamin.s.record@gmail.com>
2023-05-19 13:16:58 -0600

    Plugins: Session manager

--- lua/b/plugins/init.lua
+++ lua/b/plugins/init.lua
@@ -16,6 +16,7 @@ local plugins = function(use)
   use { 'kevinhwang91/nvim-bqf', ft = 'qf', config = [[require('b.plugins.bqf')]] } -- Better quickfix window plugin
   use { 'andymass/vim-matchup', event = 'VimEnter' } -- Enhanced % matching plugin
   use { 'folke/which-key.nvim', event = 'VimEnter', config = function() vim.defer_fn(function() require('b.plugins.which-key') end, 2000) end, } -- Key mapping plugin
+  use { 'shatur/neovim-session-manager', event = 'VimEnter', config = [[require('b.plugins.session-manager')]] } -- Manage and restore sessions
 
   use { 'nvim-telescope/telescope.nvim', config = [[require('b.plugins.telescope')]] } -- A highly extensible fuzzy finder
   use { 'nvim-telescope/telescope-symbols.nvim', after = 'telescope.nvim' } -- Enables symbol search functionality
