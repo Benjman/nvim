8eabf9b3ced3e2b20ad96a20200e8e7841758d87
Ben <benjamin.s.record@gmail.com>
2023-11-14 12:20:38 -0700

    Comments highlighting

---
+++ lua/b/plugins/todo-comment.lua
@@ -0,0 +1,16 @@
+return {
+  'folke/todo-comments.nvim',
+  opts = {
+    search = {
+      command = 'rg',
+      args = {
+        "--glob=!lib/",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+      },
+    },
+  },
+}
