f05ae13168b3d561b0f7525d0d1592d72d5f1c64
Ben <benjamin.s.record@gmail.com>
2024-02-06 13:38:04 -0700

    Removing Neovim background colors

---
+++ lua/theme.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_hl(0, "Normal", { ctermbg = NONE, guibg = NONE })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { ctermbg = NONE, guibg = NONE })
+vim.api.nvim_set_hl(0, "Signcolumn", { ctermbg = NONE, guibg = NONE })
