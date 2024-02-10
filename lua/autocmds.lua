57fc3c7d92475b8ae1186a566159b8824f1437cc
Ben <benjamin.s.record@gmail.com>
2024-02-10 16:23:56 -0700

    Telescope guard

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -107,27 +107,30 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, desc("[L]SP: [R]ename"))
     vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, desc("[L]SP: Code [A]ction"))
 
-    vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, desc("[G]oto [D]efinition"))
-    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, desc("[G]oto [R]eferences"))
-    vim.keymap.set("n", "gI", require("telescope.builtin").lsp_implementations, desc("[G]oto [I]mplementation"))
-    vim.keymap.set(
-      "n",
-      "<leader>lD",
-      require("telescope.builtin").lsp_type_definitions,
-      desc("[L]SP: Type [D]efinition")
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>lSd",
-      require("telescope.builtin").lsp_document_symbols,
-      desc("[L]SP: [S]ymbols [D]ocument")
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>lSw",
-      require("telescope.builtin").lsp_dynamic_workspace_symbols,
-      desc("[L]SP: [S]ymbols [W]orkspace")
-    )
+    if pcall(require, "telescope") then
+      vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, desc("[G]oto [D]efinition"))
+      vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, desc("[G]oto [R]eferences"))
+      vim.keymap.set("n", "gI", require("telescope.builtin").lsp_implementations, desc("[G]oto [I]mplementation"))
+      vim.keymap.set(
+        "n",
+        "<leader>lD",
+        require("telescope.builtin").lsp_type_definitions,
+        desc("[L]SP: Type [D]efinition")
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>lSd",
+        require("telescope.builtin").lsp_document_symbols,
+        desc("[L]SP: [S]ymbols [D]ocument")
+      )
+      vim.keymap.set(
+        "n",
+        "<leader>lSw",
+        require("telescope.builtin").lsp_dynamic_workspace_symbols,
+        desc("[L]SP: [S]ymbols [W]orkspace")
+      )
+    end
+
     vim.keymap.set("n", "K", vim.lsp.buf.hover, desc("Hover Documentation"))
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, desc("Signature Documentation"))
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, desc("[G]oto [D]eclaration"))
