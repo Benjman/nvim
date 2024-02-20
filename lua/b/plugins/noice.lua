daa2555b5e3f4adcb10456b267d85e43ebbee4b8
Ben <benjamin.s.record@gmail.com>
2024-02-20 11:06:51 -0700

    Removing nvim-notify

--- lua/b/plugins/noice.lua
+++ lua/b/plugins/noice.lua
@@ -7,12 +7,7 @@ return {
       -- OPTIONAL:
       --   `nvim-notify` is only needed, if you want to use the notification view.
       --   If not available, we use `mini` as the fallback
-      "rcarriga/nvim-notify",
-      config = function()
-        require("notify").setup({
-          background_colour = "#000000",
-        })
-      end,
+      -- "rcarriga/nvim-notify",
     },
   },
   event = "VeryLazy",
