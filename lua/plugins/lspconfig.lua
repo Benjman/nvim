46a74f38ee8f9dbadfb02a292b2e052a5c70eef7
Ben <benjamin.s.record@gmail.com>
2024-07-23 10:12:05 -0600

    bash language server

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        bashls = {
+          filetypes = {
+            "sh",
+            "zsh",
+          },
+        },
+      },
+    },
+  },
+}
