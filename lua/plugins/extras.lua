31fe257394a1af312c7cb997a7a1a37666716202
Ben <benjamin.s.record@gmail.com>
2024-07-23 10:12:01 -0600

    More languages

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -4,4 +4,6 @@ return {
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
 }
