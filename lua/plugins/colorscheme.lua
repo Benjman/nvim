8c2138732d28399b29ba9cbfb250f795c1e67ca6
Ben <benjamin.s.record@gmail.com>
2024-09-13 15:46:58 -0600

    Adding colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,16 @@
 return {
+  { "catppuccin/nvim" },
+  { "EdenEast/nightfox.nvim" },
+  { "folke/tokyonight.nvim" },
   { "navarasu/onedark.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  { "rebelot/kanagawa.nvim" },
+  { "rose-pine/neovim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "kanagawa",
     },
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,5 +2,6 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = function(_, opts)
     opts.defaults.file_ignore_patterns = { "build", "lib" }
+    opts.pickers.colorscheme = { ignore_builtins = true }
   end,
 }
