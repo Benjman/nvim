add6eed96637eff389c755ef2093612581508614
Ben Record <benjamin.s.record@gmail.com>
2025-01-22 13:22:43 -0700

    Removing custom telescope config after default update

--- lua/plugins/telescope.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-  opts = function(_, opts)
-    opts.defaults.file_ignore_patterns = { "build", "lib" }
-    opts.pickers.colorscheme = { ignore_builtins = true }
-  end,
-}
